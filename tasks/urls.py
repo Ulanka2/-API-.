63a926bdf971961138ec129d0565081094523883
Ulan <80949499+Ulanka2@users.noreply.github.com>
2021-09-02 15:05:56 +0600

    thre commit

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,9 +4,5 @@ from tasks.views import SurveyAPIView, QuestionAPIView
 
 urlpatterns = [
     path('', SurveyAPIView.as_view()),
-    path('question/', QuestionAPIView.as_view()),
-    # path('<int:id>/detail/', TaskDetailGenericView.as_view()),
-    # path('<int:id>/update',  TaskUpdateGenericView.as_view()),
-    # path('<int:id>/destroy',  TaskDestroyGenericView.as_view()),
-
+    path('question/', QuestionAPIView.as_view())
 ]
